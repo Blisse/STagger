613e28bfcda83d742d64b59e791119b44af4fd5a
Victor Lai <lai.victor.vl@gmail.com>
2013-02-17 23:23:06 -0500

    Feature - Set Tag
    Implemented functionality to set a tag's value.

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import os, sys
+import os, sys, logging
 import muta
 
 
+logging.basicConfig(level=logging.INFO)
+TEST_DIRECTORY = u'..\\Test Music\\'
+
 def search_valid_files(valid_extensions):
-	# @extensions	list of extensions to accept
-
-	# return		a list of filenames
-
 	filelist = []
-	for root, dirs, files in os.walk(u'.'):
+	for root, dirs, files in os.walk(TEST_DIRECTORY):
 		for f in files:
 			if f.split(u'.')[-1] in valid_extensions:
 				filelist.append(os.path.abspath(os.path.join(root.encode(u'utf-8'), f.encode(u'utf-8'))))
@@ -21,15 +20,16 @@ def search_valid_files(valid_extensions):
 if __name__ == "__main__":
 	pass
 
-
 valid_file_extensions = [u'flac', u'alac' ,u'mp3']
 
 filelist = search_valid_files(valid_file_extensions)
 
 
 for f in filelist:
+	logging.info("##FILE## "+f)
 	c = muta.audio(f)
-	print c.comment
+	logging.info("comment:"+str(c.comment))
+
 	# if isinstance(c, (list,tuple)):
 	# 	for cc in c:
 	# 		print cc
@@ -37,10 +37,9 @@ for f in filelist:
 	# 	print c
 	#print muta.audio(f).container.pprint().encode(u'utf-8')
 
-
-
 def generate_meta_master():
 	pass
+
 
 def generate_meta_slave():
 	pass
--- muta.py
+++ muta.py
@@ -2,55 +2,100 @@
 # -*- coding: UTF-8 -*-
 
 import mutagen
-# from mutagen.mp3 import MP3
-# from mutagen.id3 import ID3, TIT2
-# from mutagen.flac import FLAC
-# from mutagen.m4a import M4A
+
+from mutagen.id3 import ID3
+from mutagen.id3 import TIT2, TALB, TPE1, TPE2, COMM, TCON, TDRC, TRCK, TPOS
+
 
 import os.path as Path
 
+import logging
 
-
+read_attributes_ID3_map = {
+	"album":"TALB",
+	"albumartist":"TPE2",
+	"artist":"TPE1",
+	"date":"TDRC",
+	"discnumber":"TPOS",
+	"genre":"TCON",
+	"title":"TIT2",
+	"tracknumber":"TRCK",
+	"comment":"COMM::'eng'"
+}
+write_attributes_ID3_map = {
+	"album":"TALB",
+	"albumartist":"TPE2",
+	"artist":"TPE1",
+	"date":"TDRC",
+	"discnumber":"TPOS",
+	"genre":"TCON",
+	"title":"TIT2",
+	"tracknumber":"TRCK",
+	"comment":"COMM"
+}
+call_ID3_map = {
+	"TALB":TALB,
+	"TPE2":TPE2,
+	"TPE1":TPE1,
+	"TDRC":TDRC,
+	"TPOS":TPOS,
+	"TCON":TCON,
+	"TIT2":TIT2,
+	"TRCK":TRCK,
+	"COMM":COMM
+}
 
 class audio:
 	def __init__(self, filepath):
 		filepath = Path.realpath(filepath)
 		self.attributes = ["album","albumartist","artist","tracknumber","title","genre","date","comment"]
-		self.attributes_ID3_map = {
-			"album":"TALB",
-			"albumartist":"TPE2",
-			"artist":"TPE1",
-			"date":"TDRC",
-			"discnumber":"TPOS",
-			"genre":"TCON",
-			"title":"TIT2",
-			"tracknumber":"TRCK",
-			"comment":"COMM::'eng'"
-		}
 		self.file_path = Path.relpath(filepath)
 		self.file_type = self._find_file_type(self.file_path)
 		self.container = self._create_mutagen()
 
+		logging.debug("Mapping file attributes to audio object")
 		for attr in self.attributes:
 			setattr(self, attr, self.get_attribute(attr))
+		logging.debug("Audio object created successfully")
+		return
 
 	def get_attribute(self, attr):
 		call = attr
 		if self.file_type in ['mp3']:
-			call = self.attributes_ID3_map[attr]
+			call = read_attributes_ID3_map[attr]
 
 		try:
 			attr_ret = self.container[call]
 		except KeyError:
-			print "Could not find attribute."
 			attr_ret = ""
 
 		return attr_ret
 
+
 	def set_attribute(self, attr, value):
 		call = attr
 		if self.file_type in ['mp3']:
-			print
+			call = write_attributes_ID3_map[attr]
+			self._set_attribute_ID3(call, value)
+		elif self.file_type in ['flac','alac']:
+			self.container[call] = value
+			self.container.save()
+
+		try:
+			self.comment = value
+			logging.info("Change successful on "+call+" to "+value)
+		except:
+			raise Exception("An unexpected error has occurred")
+		return
+
+
+	def _set_attribute_ID3(self, call, value):
+		if call == "COMM":
+			self.container["COMM"] = call_ID3_map[call](encoding=3, lang=u'eng', desc='desc', text=value)
+		else:
+			self.container[call] = call_ID3_map[call](encoding=3, text=value)
+		self.container.save(v1=1)
+		return
 
 
 	def _create_mutagen(self):
@@ -62,6 +107,7 @@ class audio:
 			throw('File type not supported.')
 
 		return mutagen.File(f,easy=False)
+
 
 	def _find_file_type(self, filepath):
 		return Path.splitext(Path.basename(filepath))[1].lstrip(".").lower()
